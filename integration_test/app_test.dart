5c67ffd3d1b01c4faff413e259d9987432da1852
Nicholaus Legnard <nickyrabit@users.noreply.github.com>
2023-05-30 23:14:51 +0300

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
+ 
 
 import 'package:dynamic_navigation_drawer/main.dart' as app;
 
